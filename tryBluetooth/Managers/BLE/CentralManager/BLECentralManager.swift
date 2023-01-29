87df8da6b384f7bab736e7bd9dc130fa01515afa
abd2lrady <abd2lrady@gmail.com>
2023-01-29 11:21:43 +0200

    adding BLECentralManager and callbacks for delegate events notifications, adding BluetoothUsgaeDescription key in info.plist

---
+++ tryBluetooth/Managers/BLE/CentralManager/BLECentralManager.swift
@@ -0,0 +1,76 @@
+//
+//  CentralManager.swift
+//  tryBluetooth
+//
+//  Created by TWIAbdulrady .
+//
+
+import CoreBluetooth
+import CoreText
+
+class BLECentralManager: NSObject {
+    
+    var centralManager: CBCentralManager?
+    var peripherals: [CBPeripheral]?
+    var isPowerOn: Bool?
+    var statusUpdated: ((CBManagerState) -> Void)?
+    
+    private var discoverCompletion: ((CBPeripheral) -> Void)?
+    private var connectCompletion: ((Result<CBPeripheral, Error>) -> Void)?
+    
+    func scan(for uuid: String?,
+              discoverCompletion: @escaping ((CBPeripheral) -> Void)) {
+        
+        guard centralManager?.state == .poweredOn
+        else { return }
+        
+        self.discoverCompletion = discoverCompletion
+        
+        print("scan started")
+        
+        var cbUUID: CBUUID?
+        
+        if let uuid = uuid {
+            cbUUID = CBUUID(string: uuid)
+        }
+        
+        if let cbUUID = cbUUID {
+            centralManager?.scanForPeripherals(withServices: [cbUUID])
+        } else {
+            centralManager?.scanForPeripherals(withServices: nil)
+        }
+    }
+    
+    func connect(to peripheral: CBPeripheral,
+                 connectCompletion: @escaping ((Result<CBPeripheral, Error>) -> Void)) {
+        centralManager?.connect(peripheral)
+        self.connectCompletion = connectCompletion
+    }
+}
+
+extension BLECentralManager: CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        statusUpdated?(central.state)
+    }
+    
+    func centralManager(_ central: CBCentralManager,
+                        didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String: Any],
+                        rssi RSSI: NSNumber) {
+        peripherals?.append(peripheral)
+        discoverCompletion?(peripheral)
+    }
+    
+    func centralManager(_ central: CBCentralManager,
+                        didConnect peripheral: CBPeripheral) {
+        connectCompletion?(.success(peripheral))
+    }
+    
+    func centralManager(_ central: CBCentralManager,
+                        didFailToConnect peripheral: CBPeripheral,
+                        error: Error?) {
+        connectCompletion?(.failure(error ?? NSError(domain: "connection failure",
+                                                     code: 0)))
+    }
+}
--- tryBluetooth/Modules/first/Scenes/Main/MainViewController.swift
+++ tryBluetooth/Modules/first/Scenes/Main/MainViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class MainViewController: UIViewController {
 
+    var bleCentralManager: BLECentralManager?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print(Configuration.current)
-        print(Constants.BluetoothUUID.central)
-        print(Constants.BluetoothUUID.peripheral)
+        configCentralManager()
+        
+    }
+    
+    func configCentralManager() {
+        let cbCentralManager = CBCentralManager()
+        bleCentralManager = BLECentralManager()
+        bleCentralManager?.centralManager = cbCentralManager
+        cbCentralManager.delegate = bleCentralManager
+
+        bleCentralManager?.statusUpdated = { status in
+            
+            if status == .poweredOn {
+                print("powerOn from view controller")
+                self.bleCentralManager?.scan(for: nil) { peripheral in
+                    print("hello", peripheral)
+                }
+            }
+        }
     }
 }
